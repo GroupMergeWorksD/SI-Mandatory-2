a1bf9b085f3f4efb697587099d2508ff4340e078
Malte Bjoerklund <maltebjoerklund@gmail.com>
2026-05-26 16:10:06 +0200

    Refactor API endpoint paths to use centralized ApiPaths class for versioning

---
+++ student-course-rest-api/src/main/java/com/sirest/config/ApiPaths.java
@@ -0,0 +1,9 @@
+package com.sirest.config;
+
+public final class ApiPaths {
+    public static final String API_BASE = "/api";
+    public static final String API_V1 = API_BASE + "/v1";
+
+    private ApiPaths() {
+    }
+}
