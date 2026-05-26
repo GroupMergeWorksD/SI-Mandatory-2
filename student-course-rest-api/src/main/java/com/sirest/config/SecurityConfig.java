78ea3b64af4938f1a2ea017b65a80c45d98fb4fd
Malte Bjoerklund <maltebjoerklund@gmail.com>
2026-05-26 15:12:01 +0200

    Added openapi with swagger ui and updated postman collection with negative requests.

--- student-course-rest-api/src/main/java/com/sirest/config/SecurityConfig.java
+++ student-course-rest-api/src/main/java/com/sirest/config/SecurityConfig.java
@@ -53,7 +53,12 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
                 .authorizeHttpRequests(auth -> auth
-                       .requestMatchers("/api/auth/**").permitAll()
+                       .requestMatchers(
+                               "/api/auth/**",
+                               "/v3/api-docs/**",
+                               "/swagger-ui/**",
+                               "/swagger-ui.html"
+                       ).permitAll()
                        .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
---
+++ student-course-rest-api/swagger-ui.md
@@ -0,0 +1,5 @@
+## Swagger UI in browser
+
+When the application is running, 
+access the swagger ui in the browser
+by navigating to: `http://localhost:8081/swagger-ui.html`.
